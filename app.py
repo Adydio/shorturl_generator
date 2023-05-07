b4b3677c4170800a043709edfdff95948b39fd76
董宇坤 <11336028+ustcflask@user.noreply.gitee.com>
2023-05-07 19:02:38 +0800

    final project

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 from flask import Flask, render_template, request, redirect
+import config
+from models import URLModel
+from exts import db
 import pymysql
 from bases import Bases
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
+app = Flask(__name__)
+app.config.from_object(config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 bases = Bases()
 host = 'http://127.0.0.1:5000/'
 
-
-try:
-    connection = pymysql.connect(host='localhost',
-                                 port=3306,
-                                 user='root',
-                                 password='',
-                                 db='shorturl',
-                                 charset='utf8'
-                                 )
-except pymysql.Error:
-    raise
-
-cursor = connection.cursor()
-
-
-app = Flask('__name__')
 
 @app.route('/')
 def index():
@@ -31,15 +24,13 @@ def index():
 @app.route('/gen_short_url', methods=['POST'])
 def gen_short_url():
     long_url = request.form.get('long_url')
-    # 将长链接存入数据库
     try:
-        cursor.execute('insert into urls (url) values ("{}")'.format(long_url))
-        connection.commit()
+        long_url_add = URLModel(url=long_url)
+        db.session.add(long_url_add)
+        db.session.commit()
     except pymysql.Error:
         raise
-    # 获取id
-    last_id = cursor.lastrowid
-    # 将 id 转化为 62 进制
+    last_id = URLModel().find_id()
     encode = bases.toBase62(last_id)
     short_url = host + encode
     return render_template('index.html', short_url=short_url)
@@ -48,10 +39,9 @@ def gen_short_url():
 @app.route('/<encode_id>')
 def redirect_url(encode_id):
     id = bases.fromBase62(encode_id)
-    try:
-        cursor.execute('select url from urls where id = {}'.format(id))
-        connection.commit()
-        url = cursor.fetchone()
-        return redirect(url[0])
-    except pymysql.Error:
-        raise
+    url = URLModel().find_url(id)
+    return redirect(url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
